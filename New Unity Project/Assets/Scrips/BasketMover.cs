5bb262079f3b0d9290c613ab22cf3d9d3287d0c0
Vladosich <totm@mail.ua>
2022-02-12 23:32:47 +0200

    Цикличное движение корзины
    Переписали скрипт движения корзины

--- New Unity Project/Assets/Scrips/BasketMover.cs
+++ New Unity Project/Assets/Scrips/BasketMover.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class BasketMover : MonoBehaviour
 {
-    private float basketSpeed = 0.1f;
+    private Vector3 startPosition = new Vector3(20, 0, -10);
+    private Vector3 endPosition = new Vector3(-20, 0, -10);
+
+    private float step = 0.003f;
+    private float progress;
+    private float minProgress = 0f;
+    private float maxProgress = 1f;
+
+    private bool onEndPosition;
+
 
     private void FixedUpdate()
     {
-        MoveLeft(basketSpeed);
+        if (!onEndPosition)
+        {
+            MoveLeft(step);
+        }
+        else
+        {
+            MoveLeft(-step);
+        }
+        CheckProgress();
     }
 
-    private void MoveLeft(float speed)
+    private void MoveLeft(float step)
     {
-        transform.Translate(Vector3.left * speed);
+        transform.position = Vector3.Lerp(startPosition, endPosition, progress);
+        progress += step;
+    }
+
+    private void CheckProgress()
+    {
+        if (progress > maxProgress)
+        {
+            onEndPosition = true;
+        }
+        else if (progress < minProgress)
+        {
+            onEndPosition = false;
+        }
+
     }
 }
