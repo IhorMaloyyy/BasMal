03720ff99f2653707d75d83f45ae46b66493236a
Vladosich <totm@mail.ua>
2022-02-12 19:22:38 +0200

    Добавил начисление очков
    При попадании объекта в таргет начисляется очко

---
+++ New Unity Project/Assets/Scrips/Counter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private int score;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+        scoreText.text = "Score: " + score;
+        Destroy(other.gameObject);
+    }
+}
