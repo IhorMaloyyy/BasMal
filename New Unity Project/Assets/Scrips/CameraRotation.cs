3b5ab68def8602311e22a7e501d8bffb5e86ba4f
Vladosich <totm@mail.ua>
2022-02-17 18:21:54 +0200

    Переписал поворот камеры
    Добавил ограничения поворота камеры (для этого пришлось переписать скрипт).

--- New Unity Project/Assets/Scrips/CameraRotation.cs
+++ New Unity Project/Assets/Scrips/CameraRotation.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed;
+    private readonly float rotationSpeed = 1.5f;
+    private readonly float maxAngle = 40f;
+    private float yAngle;
+
     void Update()
     {
-        float playerInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, playerInput * rotationSpeed * Time.deltaTime, Space.World);
+        RotateInLimits();
+    }
+
+    private void RotateInLimits()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        yAngle = Mathf.Clamp(yAngle + horizontalInput * rotationSpeed, -maxAngle, maxAngle);
+        transform.eulerAngles = new Vector3(-20, yAngle, 0);
     }
 }
