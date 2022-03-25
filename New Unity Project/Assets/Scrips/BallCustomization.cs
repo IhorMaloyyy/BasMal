2dec134f102e10b2647767343572dd1a7824f717
IhorMaloyyy <ihormaloyyy@gmail.com>
2022-03-25 15:43:37 +0200

    Выбор цвета по чекбоксу

--- New Unity Project/Assets/Scrips/BallCustomization.cs
+++ New Unity Project/Assets/Scrips/BallCustomization.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallCustomization : MonoBehaviour
 {
     [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Toggle[] colorToggles;
     [SerializeField] private Material[] ballMaterials;
 
     private Renderer ballRenderer;
@@ -15,6 +17,12 @@ public class BallCustomization : MonoBehaviour
 
     public void SetBallMaterial()
     {
-        ballRenderer.material = ballMaterials[1];
+        for (int i = 0; i < colorToggles.Length; i++)
+        {
+            if (colorToggles[i].isOn)
+            {
+                ballRenderer.material = ballMaterials[i];
+            }
+        }
     }
 }
