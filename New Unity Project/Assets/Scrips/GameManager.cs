bf25c38b13eb0ba44f3f7e74543e1de9dc5d6784
IhorMaloyyy <ihormaloyyy@gmail.com>
2022-04-01 14:17:47 +0300

    Корректировка GameManager

--- New Unity Project/Assets/Scrips/GameManager.cs
+++ New Unity Project/Assets/Scrips/GameManager.cs
@@ -47,6 +47,7 @@ public class GameManager : MonoBehaviour
         {
             lastScoresText.text += $"{lastScores[i]} - {lastPlayerNames[i]} \n";
         }
+        RandomStartSong();
     }
 
     private void Start()
